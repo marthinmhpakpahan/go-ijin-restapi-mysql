aaf9aae7037d232db2f8f7df644ed03cd6b8cf70
Marthin <martin.pakpahan@edgeprop.sg>
2021-10-28 23:46:07 +0700

    Allow cors

--- helpers.go
+++ helpers.go
@@ -7,11 +7,17 @@ import (
 
 // Helper functions for respond with 200 or 500 code
 func respondWithError(err error, w http.ResponseWriter) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	w.WriteHeader(http.StatusInternalServerError)
 	json.NewEncoder(w).Encode(err.Error())
 }
 
 func respondWithSuccess(data interface{}, w http.ResponseWriter) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(data)
 }
